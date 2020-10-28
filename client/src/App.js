5c287d31cf5bae86dbd082e67d3ad08d87504935
huycntt97 <huycntt97@gmail.com>
2020-10-28 15:58:08 +0700

    Fix router

--- client/src/App.js
+++ client/src/App.js
@@ -19,8 +19,8 @@ import ListUser from "./components/manager/user/list_user";
 import BusinessCooperation from "./components/manager/cooperative/business_cooperation";
 import Footer from "./components/navigation/footer/Footer";
 import NotFound from "./components/NotFound";
-import Logout from './components/login-register/Logout';
-import * as actions from './trainRedux/action/actionAuth';
+import Logout from "./components/login-register/Logout";
+import * as actions from "./trainRedux/action/actionAuth";
 
 class App extends Component {
   componentDidMount() {
@@ -45,46 +45,45 @@ class App extends Component {
       </Switch>
     );
 
-    if (isLogin === true && role === "customer" ) {
-      routes = (
-        <Switch>
-          <Route
-            path="/login"
-            component={() => (isLogin ? <Redirect to="/" /> : <Login />)}
-          />
-          <Route path="/manager-farmer" component={ManagerFarmer} />
-          <Route path="/order-customer" component={OrderCustomer} />
-          {/* <Route path="/business-cooparetion" component={BusinessCooperation} /> */}
-          <Route path="/" exact component={Home} />
-          <Route path="/logout" component={Logout}/>
-          <Route component={NotFound} />
-          <Redirect to="/" />
-        </Switch>
-      );
-    }else{
-      routes = (
-        <Switch>
-          <Route
-            path="/login"
-            component={() => (isLogin ? <Redirect to="/" /> : <Login />)}
-          />
-          <Route path="/manager-order" component={ManagerOrder} />
-          <Route path="/list-user" component={ListUser} />
-          <Route path="/" exact component={Home} />
-          <Route path="/logout" component={Logout}/>
-          <Route component={NotFound} />
-          <Redirect to="/" />
-        </Switch>
-      );
+    if (isLogin === true) {
+      if (role === "customer ") {
+        routes = (
+          <Switch>
+            <Route
+              path="/login"
+              component={() => (isLogin ? <Redirect to="/" /> : <Login />)}
+            />
+            <Route path="/manager-farmer" component={ManagerFarmer} />
+            <Route path="/order-customer" component={OrderCustomer} />
+            {/* <Route path="/business-cooparetion" component={BusinessCooperation} /> */}
+            <Route path="/" exact component={Home} />
+            <Route path="/logout" component={Logout} />
+            <Route component={NotFound} />
+            <Redirect to="/" />
+          </Switch>
+        );
+      } else {
+        routes = (
+          <Switch>
+            <Route
+              path="/login"
+              component={() => (isLogin ? <Redirect to="/" /> : <Login />)}
+            />
+            <Route path="/manager-order" component={ManagerOrder} />
+            <Route path="/list-user" component={ListUser} />
+            <Route path="/" exact component={Home} />
+            <Route path="/logout" component={Logout} />
+            <Route component={NotFound} />
+            <Redirect to="/" />
+          </Switch>
+        );
+      }
     }
 
     if (isLogin) {
       logButton = (
         <li className="nav-item" role="presentation">
-          <Link
-            to="/logout"
-            className="nav-link"
-          >
+          <Link to="/logout" className="nav-link">
             Logout
           </Link>
         </li>
@@ -200,7 +199,7 @@ const mapStateToProps = (state) => {
   };
 };
 const mapDispatchToProps = (dispatch) => ({
-  checkUserLogin: () => dispatch(actions.checkUserLogin())
+  checkUserLogin: () => dispatch(actions.checkUserLogin()),
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
